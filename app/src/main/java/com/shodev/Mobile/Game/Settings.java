c1248a75e9e0cfcc906c8f9ff3fceaeba65e685e
jsho32 <justin.shores32@gmail.com>
2014-10-02 23:14:08 -0700

    Add files for settings menu.

---
+++ app/src/main/java/com/shodev/Mobile/Game/Settings.java
@@ -0,0 +1,13 @@
+package com.shodev.Mobile.Game;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Settings extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
