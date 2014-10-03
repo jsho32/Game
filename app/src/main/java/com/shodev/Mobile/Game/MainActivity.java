da2a296149b388922e365782ebf5bc0f528d4365
jsho32 <justin.shores32@gmail.com>
2014-10-02 23:17:01 -0700

    Set intent for settings button.

--- app/src/main/java/com/shodev/Mobile/Game/MainActivity.java
+++ app/src/main/java/com/shodev/Mobile/Game/MainActivity.java
@@ -1,23 +1,43 @@
 package com.shodev.Mobile.Game;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+    private Button settingsButton;
+    private Button timeButton;
+    private Button pointsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setButtonIntents();
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+
+    private void setButtonIntents() {
+        settingsButton = (Button) findViewById(R.id.settings_button);
+        timeButton = (Button) findViewById(R.id.time_mode_button);
+        pointsButton = (Button) findViewById(R.id.points_mode_button);
+
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Settings.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
