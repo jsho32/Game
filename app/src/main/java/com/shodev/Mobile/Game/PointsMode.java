e2ddbade56b315a86bf10523cc22f37083b61cef
jsho32 <justin.shores32@gmail.com>
2014-10-03 18:30:40 -0700

    Add Points mode.

---
+++ app/src/main/java/com/shodev/Mobile/Game/PointsMode.java
@@ -0,0 +1,12 @@
+package com.shodev.Mobile.Game;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PointsMode extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.points_mode_activity);
+    }
+}
