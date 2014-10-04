96d8f4f6edc129d040ee42c5c3c94c3e703d3505
jsho32 <justin.shores32@gmail.com>
2014-10-03 18:27:33 -0700

    Add time mode.

---
+++ app/src/main/java/com/shodev/Mobile/Game/TimeMode.java
@@ -0,0 +1,13 @@
+package com.shodev.Mobile.Game;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TimeMode extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.time_mode_activity);
+    }
+}
